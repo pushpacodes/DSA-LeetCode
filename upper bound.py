71f2efb22a5e23741be5b6ee1571f87bc189205c
kalva pushpalatha <kalvapushpaaiml50@gmail.com>
2025-09-13 18:56:56 +0530

    Add files via upload

---
+++ upper bound.py
@@ -0,0 +1,15 @@
+def upper_bound(nums,target):
+    low=0
+    ub=0
+    high=len(nums)-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]>target:
+            lb=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return lb
+nums=[1,1,1,4,5,6,7,8,8,8,9,20]
+target=1
+print(upper_bound(nums,target))
